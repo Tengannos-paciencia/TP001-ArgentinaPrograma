1e52668ac35fbb00160c75e66e5f75141f274234
DanteCar <dantecarnaghi@gmail.com>
2021-09-04 18:44:08 -0300

    se crea el proyecto y se agrega la clase Usuario

---
+++ Tests/paquete/TierraMediaTests.java
@@ -0,0 +1,14 @@
+package paquete;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TierraMediaTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/paquete/Usuario.java
@@ -0,0 +1,12 @@
+package paquete;
+
+public class Usuario {
+	private String nombre;
+	private int dineroDisponible;
+	double tiempoDisponible;
+	// tipoFavorito TipoAtraccion;
+
+	public void aceptarOfertado() {
+
+	}
+}
