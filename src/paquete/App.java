ff402c4c9938ee52653738683efcb491afc9bb96
Demián Bracamonte <87398219+dembrac@users.noreply.github.com>
2021-09-13 03:34:07 -0300

    Update App.java
    Se agregan y se ordenan las atracciones y promociones

--- src/paquete/App.java
+++ src/paquete/App.java
@@ -3,7 +3,14 @@ package paquete;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Taquilla nuevaTaquilla = new Taquilla();
+
+		nuevaTaquilla.agregarTodasLasAtracciones(ManejadorDeArchivos.obtenerAtraccionesDesdeArchivo());
+
+		nuevaTaquilla.agregarTodasLasPromociones(ManejadorDeArchivos.obtenerPromosDesdeArchivo(nuevaTaquilla));
+
+		nuevaTaquilla.ordenarLasAtracciones();
+		nuevaTaquilla.ordenarLasPromociones();
 
 	}
 
