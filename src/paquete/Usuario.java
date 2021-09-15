70f15bc5b63e9e66505115389f46cbc273f019d4
nightfallalex <reloco115@gmail.com>
2021-09-15 00:34:37 -0300

    se cambio toString de class Usuario

--- src/paquete/Usuario.java
+++ src/paquete/Usuario.java
@@ -108,8 +108,8 @@ public class Usuario extends Taquilla {
 
 	@Override
 	public String toString() {
-		return nombre + ", Tipo de atraccion favorita: " + tipoAtraccion + "\nPromociones compradas:"
-				+ promocionesAceptadas + "\nAtracciones sin promo compradas: " + atraccionesAceptadas + "\nCosto total: "
-				+ costoFinal + ", Tiempo total estimado: " + tiempoFinal + "\n";
+		return nombre + ", Tipo de atraccion favorita: " + tipoAtraccion + "\n" + ", Promociones compradas:"
+				+ promocionesAceptadas + "\n" + ", Atracciones sin promo compradas: " + atraccionesAceptadas + "\n"
+				+ " [Total a gastar:" + costoFinal + ", Tiempo total estimado: " + tiempoFinal + "]";
 	}
 }
