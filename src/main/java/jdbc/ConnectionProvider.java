46a822ed025eb83128d08846aa24bc717633550d
nightfallalex <reloco115@gmail.com>
2021-10-30 22:52:56 -0300

    se cambio nombre BD

--- src/main/java/jdbc/ConnectionProvider.java
+++ src/main/java/jdbc/ConnectionProvider.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionProvider {
-	private static String url = "jdbc:sqlite:parqueDelTerrorTest.db"; //CAMBIAR USERS POR NOMBRE
+	private static String url = "jdbc:sqlite:parqueDelTerror.db";
 	private static Connection connection;
 	
 	public static Connection getConnection () throws SQLException{
